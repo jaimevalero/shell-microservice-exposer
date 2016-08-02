2a94a3698369afa0cf0458554c8e4bf95e2d456c
jaimevalero78 <jaimevalero78@yahoo.es>
2016-08-02 19:04:49 +0200

    Add entrypoint.sh functionality

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+EXECUTOR_FULL_PATH=/root/scripts/docker-httpd-inventory/executor.sh
 
 Show_Help( )
 {
@@ -12,5 +13,21 @@ Show_Help( )
 
 # Main
 [ $# -eq 0 ] && Show_Help
-  
+
 echo Arguments are "$@"
+URL_GITHUB_REPO=$1
+NAME_GITHUB_REPO=`basename ${URL_GITHUB_REPO}`
+
+
+git clone ${URL_GITHUB_REPO} /root/scripts/${NAME_GITHUB_REPO}
+
+# Recreate directories
+cd /var/www/cgi/bin
+find -type d ${NAME_GITHUB_REPO} | sed -e 's@/root/scripts/@@' | xargs mkdir -p
+for FILE in `find -type f ${NAME_GITHUB_REPO} `
+do
+  RELATIVE_PATH=`echo $FILE |sed -e 's@/root/scripts/@@'`
+  echo "linking $RELATIVE_PATH"
+  ln -s $RELATIVE_PATH $EXECUTOR_FULL_PATH
+
+done
