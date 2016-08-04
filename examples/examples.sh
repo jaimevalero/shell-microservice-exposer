87d7b57ee29dad82268325ff2f3a904b4d48a8d2
jaimevalero78 <jaimevalero78@yahoo.es>
2016-08-04 09:18:58 +0200

    Add examples

---
+++ examples/examples.sh
@@ -0,0 +1,10 @@
+ # Stop running container
+ docker stop `docker ps | grep shell-microservice-exposer | awk '{print $1}'`
+ # Build 
+ docker build -t shell-microservice-exposer .
+ # Example of invoke container
+ docker run  -p 81:80 shell-microservice-exposer "https://github.com/jaimevalero78/itop-utilities" 
+ # Bash inside this container
+ docker exec -it `docker ps | grep shell-microservice-exposer | awk '{print $1}'` bash
+ # curl -L localhost/cgi-bin/script-name.sh
+
