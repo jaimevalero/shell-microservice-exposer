1a53690682cb99c3a7809bca1bf4edb59c806766
jaimevalero78 <jaimevalero78@yahoo.es>
2016-08-05 23:04:58 +0200

    Add jq exec

--- _executor.sh
+++ _executor.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
 
-echo "Content-type: text/html"
+
+o "Content-type: text/html"
 echo ""
-
+POST_STRING=$(cat). 
+echo "POST_STRING=$POST_STRING"
 env
-#jq -c '.|{arguments}[]' | tr -d  [ | tr -d ] | tr ',' ' '
+INPUT=`echo "$POST_STRING | jq -c '.|{arguments}[]' | tr -d  [ | tr -d ] | tr ',' ' '`
+echo INPUT=$INPUT
 
 cd /var/www/scripts
- ./${SCRIPT_NAME}
+ ./${SCRIPT_NAME} "$INPUT"
 
 
 
