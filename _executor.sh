4b91860abab03cc924a211d56a6d8954c792db28
jaimevalero78 <jaimevalero78@yahoo.es>
2016-08-06 17:24:45 +0200

    Refact executor

--- _executor.sh
+++ _executor.sh
@@ -1,15 +1,32 @@
+#!/bin/bash
 
+Receive_Arguments( )
+{
+   POST_STRING=$(cat)
+   echo "POST_STRING=$POST_STRING"
+}
 
-o "Content-type: text/html"
+Parse_Arguments( )
+{
+   INPUT=`echo "$POST_STRING" | jq -c '.|{arguments}[]' | tr -d  [ | tr -d ] | tr ',' ' '`
+   echo INPUT=$INPUT
+}
+
+Execute_Script( )
+{
+   cd /var/www/scripts
+   echo "Ejecutamos /${SCRIPT_NAME} ${INPUT}"
+   ./${SCRIPT_NAME} $INPUT
+{
+
+#MAIN
+echo "Content-type: text/html"
 echo ""
-POST_STRING=$(cat). 
-echo "POST_STRING=$POST_STRING"
-env
-INPUT=`echo "$POST_STRING | jq -c '.|{arguments}[]' | tr -d  [ | tr -d ] | tr ',' ' '`
-echo INPUT=$INPUT
 
-cd /var/www/scripts
- ./${SCRIPT_NAME} "$INPUT"
+Receive_Arguments
 
+Parse_Arguments
+
+Execute_Script
 
 
